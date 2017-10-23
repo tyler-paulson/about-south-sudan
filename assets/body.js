e383f942083df9e0b5afed67a3013728bfeccd7e
Tyler Paulson <me@tylerpaulson.com>
2017-10-23 19:25:42 -0400

    Remove Amazon links

--- assets/body.js
+++ assets/body.js
@@ -1,7 +1,7 @@
 var books = document.getElementsByClassName('books--item--buy')
 for(var i=0; i<books.length; i++) {
   books[i].addEventListener('click', function() {
-    goog_report_conversion();
+    //goog_report_conversion();
     ga('send', 'event', 'Books', 'Click', 'Unspecified Book Title');
   })
 }
